1727e0ec27afef405bf0abdf89d8adf3fc8e997e
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-04 21:38:35 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 04-Feb-2025/E - From S To T 241085.py
@@ -0,0 +1,23 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+n=int(input())
+for i in range(n):
+    s=list(input())
+    t=list(input())
+    p=list(input())
+    temp=""
+    ptr1=0
+    ptr2=0
+    while ptr2<len(t):
+        if s and t[ptr2]==s[0]:
+            ptr2+=1
+            s.remove(s[0])
+        elif t[ptr2] in p:
+            p.remove(t[ptr2])
+            ptr2+=1
+        else:
+            break
+    if len(s)==0 and ptr2==len(t):      
+        print("YES")
+    else:
+        print("NO")
