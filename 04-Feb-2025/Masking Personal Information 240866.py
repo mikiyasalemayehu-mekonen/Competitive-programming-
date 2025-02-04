efa52bad21a677cdad3810fcca29d1b2025afb2f
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-04 19:46:22 +0300

    Created 'Masking Personal Information' by A2SV Hub

---
+++ 04-Feb-2025/Masking Personal Information 240866.py
@@ -0,0 +1,31 @@
+# Problem: Masking Personal Information - https://leetcode.com/problems/masking-personal-information/description/?envType=problem-list-v2&envId=string
+
+class Solution:
+    def maskPII(self, s: str) -> str:
+        masked=''
+        if '@' in s:
+            s=s.lower()
+            name , domain = s.split('@')
+            print
+            if len(name)>1:
+                masked=name[0]+"*****"+name[-1]+'@'+domain
+            else:
+                masked=name[0]+"*****"+'@'+domain
+            return masked
+        else:
+            temp=[]
+            for char in s:
+                if  not char.isalnum():
+                    s=s.replace(char,"")
+            masked="***-***-"+s[-4:]
+            length=len(s)
+            if length>10:
+                masked="-"+masked
+                for i in range(length-10):
+                    masked="*"+masked
+                masked="+"+masked
+
+            return masked
+
+
+        
