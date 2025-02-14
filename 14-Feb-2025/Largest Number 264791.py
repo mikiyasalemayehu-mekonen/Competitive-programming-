059759580d4023084040db0aa6f30807c2c82a4f
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-14 09:13:11 +0300

    Created 'Largest Number' by A2SV Hub

---
+++ 14-Feb-2025/Largest Number 264791.py
@@ -0,0 +1,10 @@
+# Problem: Largest Number - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = sorted(nums,key= lambda item:(str(item)*10),reverse=True)
+        nums =[str(num) for num in nums]
+        if nums[0]=="0":
+            return "0"
+        return "".join(nums)
+        
