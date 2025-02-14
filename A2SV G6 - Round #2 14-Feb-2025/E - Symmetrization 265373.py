90733d049ca294fed1a915d1659a8d73ffd827a8
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-14 12:54:26 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 14-Feb-2025/E - Symmetrization 265373.py
@@ -0,0 +1,21 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+n = int(input())
+for _ in range(n):
+    m = int(input())
+    matrice = []
+    for i in range (m):
+        temp = list(map(int,input()))
+        matrice.append(temp)
+
+    count = 0
+    for row in range(m):
+        for col in range(m):
+            temp = [matrice[row][col],matrice[col][m-row-1],matrice[m-row-1][m-col-1],matrice[m-col-1][row]]
+            count = count + min(temp.count(0),temp.count(1))
+
+    print(count//4)
+    
+
+
+    
