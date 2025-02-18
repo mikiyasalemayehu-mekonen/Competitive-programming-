c62cf761aa0ec7efc27e80d3e7255ba33d9c3fcd
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-18 16:13:47 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/E - Equalizing Arrays 271844.py
@@ -0,0 +1,36 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+list1 = list(map(int,input().split()))
+k = int(input())
+list2 = list(map(int,input().split()))
+for i in range(1,n):
+    list1[i] = list1[i-1] + list1[i]
+
+for i in range(1,k):
+    list2[i] = list2[i-1] + list2[i]
+
+ptr1 = 0
+ptr2 = 0
+count = 0
+while ptr1<n and ptr2<k:
+    if list1[ptr1] == list2[ptr2]:
+        count  = count + 1
+        ptr1 = ptr1 + 1
+        ptr2 = ptr2  + 1
+    elif list1[ptr1] < list2[ptr2]:
+        ptr1  = ptr1 +  1
+    else:
+        ptr2  = ptr2 +  1
+
+if ptr1<n or ptr2<k:
+    print(-1)
+else:
+    print(count)
+
+
+
+    
+
+
+
