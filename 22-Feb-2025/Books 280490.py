b2b43d45350cebec16156d348dea965f1600ad86
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-22 21:59:58 +0300

    Created 'Books' by A2SV Hub

---
+++ 22-Feb-2025/Books 280490.py
@@ -0,0 +1,19 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+time = list(map(int, input().split()))
+
+left = 0
+current_time = 0
+max_books = 0
+
+for right in range(n):
+    current_time += time[right]
+    
+    while current_time > t:
+        current_time -= time[left]
+        left += 1
+    
+    max_books = max(max_books, right - left + 1)
+
+print(max_books)
