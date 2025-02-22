d8589ae64b73b587c64a124714b3a908dd90cf1f
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-22 23:28:24 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 22-Feb-2025/Alternating Subsequence 280594.py
@@ -0,0 +1,18 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+n=int(input())
+for i in range(n):
+    size=int(input())
+    lists=list(map(int,input().split()))
+    temp=[]
+    temp.append(lists[0])
+    index=0
+    for j in range(1,len(lists)):
+        if temp[-1] * lists[j] < 0 :
+            temp.append(lists[j])
+            index+=1
+        elif temp[index]<lists[j]:
+            temp[index]=lists[j]
+        else:
+            continue
+    print(sum(temp))
