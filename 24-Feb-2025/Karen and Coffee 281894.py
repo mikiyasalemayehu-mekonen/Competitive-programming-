b9176df6df5c0df62388af681ecbe112b73c91e2
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-24 21:24:51 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 24-Feb-2025/Karen and Coffee 281894.py
@@ -0,0 +1,31 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n , k ,q = map(int,input().split())
+tempreature = [0] * 200002
+
+ranges = [] 
+for i in range(n):
+    t1 , t2 = map(int,input().split())
+    tempreature[t1] = tempreature[t1] + 1
+    tempreature[t2+1] = tempreature[t2+1] - 1
+
+for i in range(1,200002):
+    
+    tempreature[i] = tempreature[i-1] + tempreature[i]
+
+tempre_freq = [0] * 200002
+
+for i in range(1,200002):
+    if tempreature[i] >=k:
+        tempre_freq[i] = tempre_freq[i-1] + 1
+    else:
+        tempre_freq[i] = tempre_freq[i-1]
+
+
+for i in range(q):
+    t1 , t2 = map(int,input().split())
+    print(tempre_freq[t2] -tempre_freq[t1-1])
+
+
+
+
