21e43cb04f6f62d10eccba54d66c62c95bedc2db
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-28 15:32:14 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/D - Minimize the Distance 286795.py
@@ -0,0 +1,24 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+nums = list(map(int,input().split())) 
+nums.sort()                   
+
+prefix_sum = [0] * n
+suffix_sum = [0] * n
+
+for i in range(1, n):
+    prefix_sum[i] = prefix_sum[i - 1] + (nums[i] - nums[i - 1]) * i
+
+for i in range(n - 2, -1, -1):
+    suffix_sum[i] = suffix_sum[i + 1] + (nums[i + 1] - nums[i]) * (n - i - 1)
+min_cost = float('inf')
+best_index = 0
+
+for i in range(n):
+    total_cost = prefix_sum[i] + suffix_sum[i]
+    if total_cost < min_cost:
+        min_cost = total_cost
+        best_index = i
+
+print(nums[best_index])  
