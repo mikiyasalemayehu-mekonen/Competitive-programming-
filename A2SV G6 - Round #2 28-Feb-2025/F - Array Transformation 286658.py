71707df525cd544d4fadd7cea63d1857b1ce4315
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-28 10:37:47 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 28-Feb-2025/F - Array Transformation 286658.py
@@ -0,0 +1,24 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+
+from collections import Counter
+
+
+n = int(input())
+for _ in range(n):
+    size = int(input())
+    nums = list(map(int,input().split()))
+    minimum = size
+    count = Counter(nums)
+    max_count = max(count.values())
+    freq = list(set(count.values()))
+    for num in freq:
+        temp = 0
+        for key,value in count.items():
+            if value <num:
+                temp = temp + value 
+            elif value>num:
+                temp = temp + value - num
+        minimum = min(minimum,temp)
+                
+    print(minimum)
