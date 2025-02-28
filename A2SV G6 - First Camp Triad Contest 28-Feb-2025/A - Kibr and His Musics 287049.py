b9cbb6ccbabf6d223b0ff501540b9c3de73d6996
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-02-28 19:11:30 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 28-Feb-2025/A - Kibr and His Musics 287049.py
@@ -0,0 +1,17 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+n  , q = map(int,input().split())
+prefix = [0]*(n+1)
+for i in range(1,n+1):
+    c,t = map(int,input().split())
+    prefix[i] = prefix[i-1] + (c*t)
+prefix = prefix[1:]
+queries = list(map(int,input().split()))
+
+index = 0
+for qr in queries:
+    while index<n:
+        if qr<=prefix[index]:
+            print(index+1)
+            break
+        index = index + 1
