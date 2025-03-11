afd188aeb6fe1a93c42b46813066ad7523de95bd
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-11 09:28:51 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 11-Mar-2025/Red and Blue 301107.py
@@ -0,0 +1,14 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+from itertools import accumulate
+
+
+n = int(input())
+for _ in range(n):
+    r = int(input())
+    red = list(map(int,input().split()))
+    b = int(input())
+    blue = list(map(int,input().split()))
+    max_red = max(0,max(list(accumulate(red))))
+    max_blue = max(0,max(list(accumulate(blue))))
+    print(max_blue+max_red)
