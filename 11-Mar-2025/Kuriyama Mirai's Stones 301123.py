f6b6d45c72c099fffecb8a1cae862cc368b1c3d0
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-11 09:46:49 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 11-Mar-2025/Kuriyama Mirai's Stones 301123.py
@@ -0,0 +1,21 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+from itertools import accumulate
+
+
+n = int(input())
+stones = list(map(int,input().split()))
+prefix_sum = list(accumulate(stones))
+stones.sort()
+sorted_prefix_sum = list(accumulate(stones))
+m = int(input())
+for _ in range(m):
+    t,l,r = map(int,input().split())
+    if t==1 and l-2>=0:
+        print(prefix_sum[r-1]-prefix_sum[l-2])
+    elif t==1:
+        print(prefix_sum[r-1])
+    if t==2 and l-2>=0:
+        print(sorted_prefix_sum[r-1]-sorted_prefix_sum[l-2])
+    elif t==2:
+        print(sorted_prefix_sum[r-1])
