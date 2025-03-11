486e47ed6c5878e36d5cce93feb1c2506b393759
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-11 23:01:19 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/D - Meklit's Chat Screen 303328.py
@@ -0,0 +1,25 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+
+n , k = map(int,input().split())
+message =  list(map(int,input().split()))
+queue = deque()
+count = 0
+seen = set()
+for i in range(n):
+    if queue and message[i] not in seen and len(queue)<k:
+        queue.appendleft(message[i])
+        count = count + 1
+        seen.add(message[i])
+    elif queue and message[i] not in seen and len(queue)==k:
+        temp = queue.pop()
+        queue.appendleft(message[i])
+        seen.remove(temp)
+        seen.add(message[i])
+    elif not queue:
+        queue.appendleft(message[i])
+        count = count + 1
+        seen.add(message[i])
+print(count)
+print(*queue)
