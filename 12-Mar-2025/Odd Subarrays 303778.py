fcd56affa86258eba5d0921a0b5c6dd3bf10ee5d
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-12 14:30:13 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 12-Mar-2025/Odd Subarrays 303778.py
@@ -0,0 +1,17 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+#miki
+def maps():
+    return map(int,input().split())
+n = int(input())
+for _ in range(n):
+    size = int(input())
+    num = list(maps())
+    count = 0
+    i = 0
+    while i <size-1:
+        if num[i]>num[i+1]:
+            count = count + 1
+            i = i + 1
+        i = i + 1
+    print(count)
