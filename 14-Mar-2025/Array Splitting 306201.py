8cd7803764a48f40bf4d1558d115d765a6e9073c
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-14 10:34:03 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 14-Mar-2025/Array Splitting 306201.py
@@ -0,0 +1,11 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n , k= map(int,input().split())
+stones = list(map(int,input().split()))
+if k == n:  
+    print(0)
+    exit()
+
+gaps = [stones[i] - stones[i - 1] for i in range(1, n)]
+gaps.sort(reverse=True)
+print(sum(gaps) - sum(gaps[:k - 1]))
