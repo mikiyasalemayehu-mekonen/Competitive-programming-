2db1023eca4ecc174c86fb5018992496269b517e
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-20 14:37:36 +0300

    Created 'C - Penalty Shootout' by A2SV Hub

---
+++ A2SV G6 - Round #7 20-Mar-2025/C - Penalty Shootout 313137.py
@@ -0,0 +1,57 @@
+# Problem: C - Penalty Shootout - https://codeforces.com/gym/596141/problem/C
+
+n = int(input())
+for _  in range(n):
+    penality = list(input())
+    team1 = 0
+    team2 = 0
+    i = 0
+    while i<10:
+        if  penality[i]=="1" and i%2==0 :
+            team1 = team1 + int(penality[i])
+        elif  penality[i]=="1" and i%2!=0:
+            team2 = team2 + int(penality[i])
+        elif penality[i]=="?":
+            if i%2==0:
+                team1 = team1 + 1
+        if i%2==0 and (team1)>(team2 + (10-i)//2 ):
+            
+            break
+        if i%2==0 and (team1 +  ((10-i)//2) - 1 )<(team2):
+            break
+        if i%2!=0 and (team1 + (10-i-1)//2)<(team2):
+            break
+        if i%2!=0 and (team1)>(team2+((10-i-1)//2)):
+            break
+        i = i + 1
+    
+        
+
+    ans1 = i + 1 if i<10 else 10
+    team1 = 0
+    team2 = 0
+    i = 0
+    while i<10:
+        if  penality[i]=="1" and i%2==0 :
+            team1 = team1 + int(penality[i])
+        elif  penality[i]=="1" and i%2!=0:
+            team2 = team2 + int(penality[i])
+        elif penality[i]=="?":
+            if i%2!=0:
+                team2 = team2 + 1
+        if i%2==0 and (team1)>(team2 + (10-i)//2 ):
+            
+            break
+        if i%2==0 and (team1 +  ((10-i)//2) - 1 )<(team2):
+            break
+        if i%2!=0 and (team1 + (10-i-1)//2)<(team2):
+            break
+        if i%2!=0 and (team1)>(team2+((10-i-1)//2)):
+            break
+        i = i + 1
+    
+        
+
+    ans2 = i + 1 if i<10 else 10
+
+    print(min(ans1,ans2))
