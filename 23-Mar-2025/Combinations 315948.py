0f0f425b9d68c98c079897374777d2149f869ff4
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-03-23 09:20:47 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 23-Mar-2025/Combinations 315948.py
@@ -0,0 +1,15 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        def backtrack(i,store):
+            if len(store)==k:
+                result.append(store[:])
+                return
+            for j in range(i+1,n+1):
+                store.append(j)
+                backtrack(j,store)
+                store.pop()
+        backtrack(0,[])
+        return result
