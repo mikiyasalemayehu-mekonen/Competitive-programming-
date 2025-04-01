8fd0bbba89e49a1aebb17e39a8d9bbea8badb8f5
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-04-01 19:46:45 +0300

    Created 'Sort List' by A2SV Hub

---
+++ 01-Apr-2025/Sort List 323439.py
@@ -0,0 +1,41 @@
+# Problem: Sort List - https://leetcode.com/problems/sort-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def merge(self, left: ListNode, right: ListNode) -> ListNode:
+        dummy = ListNode()
+        tail = dummy
+
+        while left and right:
+            if left.val <= right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+
+        tail.next = left if left else right  # Attach remaining nodes
+        return dummy.next
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second_half = slow.next
+        slow.next = None    
+       
+        left_sorted = self.sortList(head)
+        right_sorted = self.sortList(second_half)
+        return self.merge(left_sorted, right_sorted)
+
+
+
+ 
