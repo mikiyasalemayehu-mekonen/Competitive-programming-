9e3f7afe437934e4db8b2b8c38423ed966d77604
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-04-06 09:59:32 +0300

    Created 'Longest Nice Substring' by A2SV Hub

---
+++ 06-Apr-2025/Longest Nice Substring 327166.py
@@ -0,0 +1,13 @@
+# Problem: Longest Nice Substring - https://leetcode.com/problems/longest-nice-substring/
+
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if len(s) < 2:
+            return ""
+        char_set = set(s)
+        for i, c in enumerate(s):
+            if c.swapcase() not in char_set:
+                left = self.longestNiceSubstring(s[:i])
+                right = self.longestNiceSubstring(s[i+1:])
+                return left if len(left) >= len(right) else right
+        return s
