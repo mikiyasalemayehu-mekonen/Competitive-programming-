1cf1ec41654ede2ca28090521b6d4259820b3350
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-04-09 19:22:48 +0300

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 09-Apr-2025/Find the Town Judge 329941.py
@@ -0,0 +1,22 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        pos_ans = [i for i in range(1,n+1)]
+        adj = defaultdict(list)
+        for u,v in trust:
+            adj[u].append(v)
+            if u in pos_ans:
+                pos_ans.remove(u)
+        if len(pos_ans)!=1:
+            return -1
+        print(pos_ans)
+        for i in range(1,n+1):
+            if i==pos_ans[0]:
+                continue
+            elif pos_ans[0] not in adj[i]:
+                break
+        else:
+            return pos_ans[0]
+
+        return -1
