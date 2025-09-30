976f7eb8f4b6eb3602dd42293eb1b66f47669728
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-09-30 14:58:04 +0300

    Created 'Decode Ways' by A2SV Hub

---
+++ 30-Sep-2025/Decode Ways 397905.py
@@ -0,0 +1,21 @@
+# Problem: Decode Ways - https://leetcode.com/problems/decode-ways/
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        memo ={}
+        def dp(i):
+            if i ==n:
+                return 1
+            if i in memo:
+                return memo[i]
+            ans = 0
+            for j in range(i+1,min(i+3,n+1)):
+                if s[i]=="0":
+                    break
+                if int(s[i:j])<=26 and int(s[i:j])>0 :
+                    ans += dp(j)
+            memo[i] = ans
+            return memo[i]
+
+        return dp(0)
