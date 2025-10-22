52cf0bb41fdaaacfee4472770438c4c67b8d199d
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-10-22 19:03:21 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 22-Oct-2025/Dreamoon and Stairs 405102.py
@@ -0,0 +1,44 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+
+def in_str() -> str: return sys.stdin.readline().strip()
+def t_int() -> int: return int(sys.stdin.readline().strip())
+def map_int(): return map(int, sys.stdin.readline().strip().split())
+def t_list() -> list: return list(map(int, sys.stdin.readline().strip().split()))
+def in_strings(): return sys.stdin.readline().strip().split()
+def in_string_list() -> list: return list(sys.stdin.readline().strip())
+def in_matrix(n: int) -> list: return [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+
+def solve():
+    n,m = map_int()
+    if n<m:
+        print(-1)
+        return
+    else:
+        if n==m:
+            print(n)
+            return
+
+        temp = n//2 
+        cnt = temp
+        temp = temp + 1 if n%2!=0 else temp
+        while cnt!=0:
+            if temp%m==0:
+                print(temp)
+                return
+            temp = temp + 1
+            cnt = cnt - 1
+        print(-1)
+
+    
+    
+    
+
+def main():
+    solve()
+
+if __name__ == "__main__":
+    main()
