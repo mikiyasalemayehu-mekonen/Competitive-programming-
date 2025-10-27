320aa68b39079b3c17e39e1ff69bbf0767bc50c0
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2025-10-27 16:45:03 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 27-Oct-2025/Destroying Bridges 405942.py
@@ -0,0 +1,31 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+
+def in_str() -> str: return sys.stdin.readline().strip()
+def t_int() -> int: return int(sys.stdin.readline().strip())
+def map_int(): return map(int, sys.stdin.readline().strip().split())
+def t_list() -> list: return list(map(int, sys.stdin.readline().strip().split()))
+def in_strings(): return sys.stdin.readline().strip().split()
+def in_string_list() -> list: return list(sys.stdin.readline().strip())
+def in_matrix(n: int) -> list: return [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+
+def solve():
+    t = t_int()
+    for i in range(t):
+        n,k  = map_int()
+        if k>=n-1:
+            print(1)
+        else:
+            print(n)
+
+
+
+
+def main():
+    solve()
+
+if __name__ == "__main__":
+    main()
